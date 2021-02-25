0521f0c66c4207e587daf5349771a5553e3a33fc
KimGeunUk <74355042+KimGeunUk@users.noreply.github.com>
2021-02-25 18:33:22 +0900

    코딩테스트 연습

---
+++ 두 정수 사이의 합.py
@@ -0,0 +1,15 @@
+def solution(a,b):
+    answer = 0
+
+    if a<b:
+        for i in range(a,b+1):
+            answer += i
+    else:
+        for i in range(b,a+1):
+            answer += i
+
+    return answer
+
+print(solution(3,5))
+print(solution(3,3))
+print(solution(5,3))
